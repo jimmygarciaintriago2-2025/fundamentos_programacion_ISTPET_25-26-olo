dbe7cf0a25691b7d3aa1336dd23c51e52a06e8e1
jimmy garcia <jimmygarciaintriago2@gmail.com>
2025-11-19 22:14:14 -0500

    hola

---
+++ miprsentacion/src/miprsentacion/mipresentacion.java
@@ -0,0 +1,11 @@
+package miprsentacion;
+
+public class mipresentacion {
+
+	public static void main(String[] args) {
+		System.out.println("Mi nombre es Jimmy Garcia y tengo 27 años");
+
+
+	}
+
+}
